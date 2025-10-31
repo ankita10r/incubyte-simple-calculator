1afcf784c4c4194fd283878dc8641f41836217c7
Ankita Ranjan <ankita@fynarfin.io>
2025-10-31 13:30:15 +0530

    first commit

---
+++ src/main/java/com/example/incubyte_string_calculator/IncubyteStringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.incubyte_string_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteStringCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteStringCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/incubyte_string_calculator/IncubyteStringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incubyte_string_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteStringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
